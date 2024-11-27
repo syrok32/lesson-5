5272ecc0aaeeec2f6cd7feb02b955c6d0b9fd019
syrok32 <stepstepan2@gmail.com>
2024-11-27 23:21:29 +0300

    add dec

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,6 @@ public class Main {
         }
         int deliveryDistance = 115;
         int countDay = 1;
-
         if (deliveryDistance <= 20){
             System.out.println("Потребуется дней: " + countDay);
         }else {
