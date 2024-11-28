f086e3426c0d4a77bc3f68acfb9fd2fccf4c6c4b
syrok32 <stepstepan2@gmail.com>
2024-11-28 09:29:51 +0300

    commit new main

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,58 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+
         int clientOS = 1;
-        String getAnser = clientOS == 1 ? "Установите версию приложения для Android по ссылке" : "Установите версию приложения для iOS по ссылке";
-        System.out.println(getAnser);
+
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+
+        System.out.println(2);
 
         int clientDeviceYear = 2015;
-        String getAnserYear = clientOS == 1 ?
-                clientDeviceYear >= 2015 ? "Установите обычную версию приложения для Android по ссылке" : "Установите облегченную версию приложения для Android по ссылке"
-                : clientDeviceYear >= 2015 ? "Установите обычную версию приложения для IOS по ссылке" : "Установите облегченную версию приложения для IOS по ссылке";
-        System.out.println(getAnserYear);
+        String yearManufacture = clientOS == 1 ? clientDeviceYear >= 2015 ? "Установите обычную версию приложения для Android по ссылке" : "Установите облегченную версию приложения для Android по ссылке" : clientDeviceYear >= 2015 ? "Установите обычную версию приложения для IOS по ссылке" : "Установите облегченную версию приложения для IOS по ссылке";
+        System.out.println(yearManufacture);
 
+        System.out.println(3);
         int Year = 2021;
-        boolean TR = (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
-        if (TR) {
+
+        boolean leapYear = (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+        if (leapYear) {
+
             System.out.println(Year + " Высокосный");
         } else {
             System.out.println(Year + " Невысокосный");
         }
+
+        System.out.println(4);
+
         int deliveryDistance = 115;
         int countDay = 1;
+
+
+
         if (deliveryDistance <= 20){
             System.out.println("Потребуется дней: " + countDay);
-        }else {
+        } else {
             countDay += 1;
             if (deliveryDistance <= 60) {
 
                 System.out.println("Потребуется дней: " + countDay);
-            }else{
-                    countDay +=1;
-                    if (deliveryDistance <= 100){
+            } else {
+                countDay += 1;
+                if (deliveryDistance <= 100) {
 
-                        System.out.println("Потребуется дней: " + countDay);
-                    }else System.out.println("доставки нет");
-                }
+                    System.out.println("Потребуется дней: " + countDay);
+                } else System.out.println("доставки нет");
+            }
 
         }
-
+        System.out.println(5);
         int monthNumber = 12;
         switch (monthNumber) {
             case 1:
@@ -83,7 +99,6 @@ public class Main {
                 System.out.println("Некорректный номер месяца");
                 break;
         }
-
 
 
     }
